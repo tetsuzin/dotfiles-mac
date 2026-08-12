0876ab03d3ffe2f405c62c487d389d5df66554b4
tetsuzin <230069+tetsuzin@users.noreply.github.com>
2026-08-12 13:44:13 +0900

    home-managerのバックアップ拡張子を設定
    既存ファイルと衝突した際にactivationがエラーで止まらず
    .backup として退避されるようにする。

--- nix/nix-darwin/_main.nix
+++ nix/nix-darwin/_main.nix
@@ -8,6 +8,7 @@
   home-manager = {
     useGlobalPkgs = true;
     useUserPackages = true;
+    backupFileExtension = "backup";
     users.${user} = import ../home-manager/_main.nix;
     extraSpecialArgs = { inherit user; };
   };
